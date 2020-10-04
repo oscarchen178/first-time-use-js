0f8d695e9caeae7c3a791f339af144a66e0d3415
oscarchen178 <65813281+oscarchen178@users.noreply.github.com>
2020-10-05 00:35:32 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// hi javascript
+console.log('hello World');
+const name = "YAuanhan"; //unchangeable variable
+let firstname = 'Oscar', lastname = 'Chen';
+firstname = "Yuanhan";
+console.log(firstname);
+let firstbool = true;
+let person = {
+    name: 'Oscar',
+    age: 19
+};
+person.age += 1;
+person['name'] = 'Simon';
+console.log(person);
+
+let array = [1,2,3,4];
+console.log(array.length);

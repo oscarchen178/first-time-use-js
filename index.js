f77dfc81d737b1c827810e4b568ae33974839a63
oscarchen178 <65813281+oscarchen178@users.noreply.github.com>
2020-10-05 02:07:57 +0800

    Add files via upload
    Funcion practice

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ let oscar = {
     the_guy_infrontof: undefined,
     age: 19,
     greet: function() {
-        console.log(this.name + ' says hello ' + this.the_guy_infrontof + '!')
+        console.log(this.name + ' says hello ' + 
+        this.the_guy_infrontof + '!')
     }
 }
 oscar.the_guy_infrontof = 'Cindy';
@@ -23,12 +24,30 @@ function createCircle(radius) {
 const circle = createCircle(2);
 circle.draw();
 
+for (let key in circle) {
+    if (typeof circle[key] !== 'function')
+        console.log(key, circle[key]);
+}
+
 //Constructor Function
 function Circle(radius) {
+    let defaultlocation = { x: 0, y: 0 };
     this.radius = radius;
     this.draw = function() {
         console.log('draw');
     }
+    Object.defineProperty(this, 'defaultlocation',
+        { get: function() {
+            return defaultlocation;
+        },
+        set: function(value){
+            if (!value.x || !value.y)
+                throw new Error('Invalid location.');
+            defaultlocation = value;
+        }
+    });
 }
 const anothercircle = new Circle(1);
+anothercircle.defaultlocation = {x: 1, y: 1};
+console.log(anothercircle.defaultlocation);
 anothercircle.draw();
